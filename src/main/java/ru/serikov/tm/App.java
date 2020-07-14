6174fd0e1e22b36beb3f1329004b718945fd7825
Sergei Serikov <sserikov90@yandex.ru>
2020-07-14 23:44:29 +0300

    добавлена главная петля приложения

--- src/main/java/ru/serikov/tm/App.java
+++ src/main/java/ru/serikov/tm/App.java
@@ -1,57 +1,80 @@
 package ru.serikov.tm;
 
+import java.util.Scanner;
+
 import static ru.serikov.tm.constant.TerminalConst.*;
 
 /**
  * Тестовое приложение
  */
-
 public class App {
 
     public static void main(final String[] args) {
         run(args);
         displayWelcome();
+        process();
+    }
+
+    public static void process() {
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!CMD_EXIT.equals(command)) {
+            command = scanner.nextLine();
+            run(command);
+            System.out.println();
+        }
     }
 
     private static void run (final String[] args) {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
+        final int  result = run(param);
+        System.exit(result);
+    }
+
+    private static int run (final String param) {
+        if (param == null || param.isEmpty()) return -1;
         switch (param) {
-            case CMD_VERSION: displayVersion();
-            case CMD_ABOUT: displayAbout();
-            case CMD_HELP: displayHelp ();
-            default: displayError();
+            case CMD_VERSION: return displayVersion();
+            case CMD_ABOUT: return displayAbout();
+            case CMD_HELP: return displayHelp ();
+            case CMD_EXIT: return Exit ();
+            default: return displayError();
         }
     }
 
-    private static void displayError() {
+    private static int displayError() {
         System.out.println("Error! Unknown program argument...");
-        System.exit(-1);
+        return -1;
     }
 
     private static void displayWelcome() {
         System.out.println("** WELCOME TO TASK MANAGER **");
     }
 
-    private static void displayHelp() {
+    private static int displayHelp() {
         System.out.println("version - display application version");
         System.out.println("about - display developer info");
         System.out.println("help - display list of commands");
-        System.exit(0);
+        System.out.println("exit - terminate console application");
+        return 0;
     }
 
-    private static void displayAbout() {
+    private static int displayAbout() {
         System.out.println("Sergey Serikov");
         System.out.println("serikov_sy@nlmk.com");
-        System.exit(0);
+        return 0;
     }
 
-    private static void displayVersion() {
+    private static int displayVersion() {
         System.out.println("1.0.0");
+        return 0;
+    }
+
+    private static int Exit() {
         System.exit(0);
+        return 0;
     }
 
 }
-
-
--- src/main/java/ru/serikov/tm/constant/TerminalConst.java
+++ src/main/java/ru/serikov/tm/constant/TerminalConst.java
@@ -5,5 +5,6 @@ public class TerminalConst {
     public static final String CMD_HELP = "help";
     public static final String CMD_VERSION = "version";
     public static final String CMD_ABOUT = "about";
+    public static final String CMD_EXIT = "exit";
 
 }
